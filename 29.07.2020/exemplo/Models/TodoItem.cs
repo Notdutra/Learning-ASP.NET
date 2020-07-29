c337c68627ed28100742b58eca21a282e14a73c6
NotDutra <arthurdutra@live.ca>
2020-07-29 17:19:54 -0300

    removed because my professor is trash

--- 29.07.2020/exemplo/Models/TodoItem.cs
+++
@@ -1,7 +0,0 @@
-namespace exemplo {
-    public class TodoItem {
-        public long Id { get; set; }
-        public string Name { get; set; }
-        public bool IsCompleted { get; set; }
-    }
-}
