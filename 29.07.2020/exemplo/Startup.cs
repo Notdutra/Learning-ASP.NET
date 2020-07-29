63268e9b84d001f3582de756419ecae2fd773f89
NotDutra <arthurdutra@live.ca>
2020-07-29 17:18:58 -0300

    h

--- 29.07.2020/exemplo/Startup.cs
+++ 29.07.2020/exemplo/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace exemplo {
     public class Startup {
@@ -21,6 +23,9 @@ namespace exemplo {
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices (IServiceCollection services) {
+            services.AddDbContext<TodoContext>(opt => {
+                opt.UseSqlServer;
+            });
             services.AddControllers ();
         }
 
