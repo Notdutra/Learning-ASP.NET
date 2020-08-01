2df3b1c175c28e0269ff65d8fee1bf039cad665d
NotDutra <arthurdutra@live.ca>
2020-08-01 20:56:14 -0300

    Update

--- 31.07.2020/Exemplo da aula/ContosoUniversity-Backend-master/ContosoUniversity.Model/ViewModels/AssignedCourseViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ContosoUniversity.Model.ViewModels
-{
-    public class AssignedCourseViewModel
-    {
-        public int CourseID { get; set; }
-        public string Title { get; set; }
-        public bool Assigned { get; set; }
-    }
-}
