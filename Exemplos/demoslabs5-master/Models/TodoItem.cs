93631e345328eb83f6928ce67617e387aa7e984a
NotDutra <arthurdutra@live.ca>
2020-08-02 21:34:22 -0300

    Update

--- Exemplos/demoslabs5-master/Models/TodoItem.cs
+++
@@ -1,9 +0,0 @@
-namespace demoslabs5.models
-{
-    public class TodoItem
-    {
-        public long Id {get; set;}
-        public string Name {get; set;}
-        public bool IsCompleted {get; set;}
-    }
-}
--- Exemplos/demoslabs5-master/README.md
+++
@@ -1,2 +0,0 @@
-# demoslabs5
-
