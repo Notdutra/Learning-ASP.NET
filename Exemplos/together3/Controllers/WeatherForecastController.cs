5167ef891202036bf45d58a03d4c8551709bc30c
NotDutra <arthurdutra@live.ca>
2020-08-03 21:12:01 -0300

    Update

--- Exemplos/together3/Controllers/WeatherForecastController.cs
+++ Exemplos/together3/Controllers/WeatherForecastController.cs
@@ -5,35 +5,38 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-namespace together3.Controllers
-{
+namespace together3.Controllers {
     [ApiController]
-    [Route("[controller]")] // "[controller]" vai ser WeatherForecast ignorando o "Controller"
-    public class WeatherForecastController : ControllerBase
-    {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    [Route ("[controller]")] // "[controller]" vai ser WeatherForecast ignorando o "Controller"
+    public class WeatherForecastController : ControllerBase {
+        private static readonly string[] Summaries = new [] {
+            "Freezing",
+            "Bracing",
+            "Chilly",
+            "Cool",
+            "Mild",
+            "Warm",
+            "Balmy",
+            "Hot",
+            "Sweltering",
+            "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
-        {
+        public WeatherForecastController (ILogger<WeatherForecastController> logger) {
             _logger = logger;
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+        public IEnumerable<WeatherForecast> Get () {
+            var rng = new Random ();
+            return Enumerable.Range (1, 5).Select (index => new WeatherForecast {
+                    Date = DateTime.Now.AddDays (index),
+                        TemperatureC = rng.Next (-20, 55),
+                        Summary = Summaries[rng.Next (Summaries.Length)]
+                })
+                .ToArray ();
         }
     }
 }
